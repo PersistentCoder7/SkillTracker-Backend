9a50b257818e49f22678b4c0031f5b3345cc882f
Prabhudarshan Singh <Prabhu25.1975@gmail.com>
2022-11-01 22:46:19 +0530

    Register the EventBus  using IOC container.

---
+++ SkillTracker.Infrastructure.IoC/DependencyContainer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using SkillTracker.Domain.Core.Bus;
+using SkillTracker.Infrastructure.Bus;
+
+namespace SkillTracker.Infrastructure.IoC
+{
+    public  class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            //Domain Bus
+            services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}
