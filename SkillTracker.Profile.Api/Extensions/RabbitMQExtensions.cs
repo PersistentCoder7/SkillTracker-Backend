30460b101322c4fbac974dafec9045e758616bd2
Prabhugurudarshan Singh <prabhu25.1975@gmail.com>
2023-05-18 11:31:12 +0530

    Updated these files for Azure service bus.

--- SkillTracker.Profile.Api/Extensions/RabbitMQExtensions.cs
+++ SkillTracker.Profile.Api/Extensions/RabbitMQExtensions.cs
@@ -4,6 +4,7 @@ using SkillTracker.Profile.Api.Utils;
 using SkillTracker.Profile.Application.Interfaces;
 using SkillTracker.Profile.Infrastructure.Consumers;
 using Microsoft.Extensions.Logging; // Add this line
+using Azure.Messaging.ServiceBus;
 
 namespace SkillTracker.Profile.Api.Extensions
 {
@@ -17,37 +18,66 @@ namespace SkillTracker.Profile.Api.Extensions
 
             logger.LogInformation($"SB connection string: {connectionString}"); // Log the parsed connection string
 
-            var parser = new RabbitMqConnectionStringParser();
-            var rabbitConfig = parser.ParseConnectionString(connectionString);
-
-            logger.LogInformation($"Host: {rabbitConfig.Host}, Port:{rabbitConfig.Port}"); // Log the parsed connection string
-            builder.Services.AddMassTransit(x =>
+            if (builder.Environment.EnvironmentName.Equals("Cloud")) // Check if message broker is "Cloud"
             {
-                x.SetKebabCaseEndpointNameFormatter();
-                x.AddConsumer<AddProfileConsumer>(x =>
+                builder.Services.AddMassTransit(x =>
                 {
-
-                });
-                x.AddConsumer<UpdateProfileConsumer>(x =>
-                {
-
-                });
-                x.AddConsumer<RefreshCacheEventConsumer>(x =>
-                {
-
-                });
-                x.UsingRabbitMq((context, cfg) =>
-                {
-                    cfg.Host(rabbitConfig.Host, "/", px =>
+                   ConfigureSBConsumers(x);
+                   x.UsingAzureServiceBus((context, cfg) =>
                     {
-                        px.Username(rabbitConfig.Username);
-                        px.Password(rabbitConfig.Password);
+                        cfg.Host(connectionString);
+                        // Configure the queues for your consumers
+                        cfg.ReceiveEndpoint("add-profile-queue", e =>
+                        {
+                            e.Consumer<AddProfileConsumer>(context);
+                            e.AutoStart = true;
+                        });
+
+                        cfg.ReceiveEndpoint("update-profile-queue", e =>
+                        {
+                            e.Consumer<UpdateProfileConsumer>(context);
+                            e.AutoStart = true;
+                        });
+
+                        cfg.ReceiveEndpoint("refresh-cache-queue", e =>
+                        {
+                            e.Consumer<RefreshCacheEventConsumer>(context);
+                            e.AutoStart = true;
+                        });
+                        cfg.ConfigureEndpoints(context);
                     });
-                    cfg.ConfigureEndpoints(context);
-
+                    
                 });
-            });
+            }
+            else // Default to RabbitMQ if message broker is not "Cloud"
+            {
+                var parser = new RabbitMqConnectionStringParser();
+                var rabbitConfig = parser.ParseConnectionString(connectionString);
 
+                logger.LogInformation($"Host: {rabbitConfig.Host}, Port: {rabbitConfig.Port}"); // Log the parsed connection string
+                builder.Services.AddMassTransit(x =>
+                {
+                    ConfigureSBConsumers(x);
+                    x.UsingRabbitMq((context, cfg) =>
+                    {
+                        cfg.Host(rabbitConfig.Host, "/", px =>
+                        {
+                            px.Username(rabbitConfig.Username);
+                            px.Password(rabbitConfig.Password);
+                        });
+                        cfg.ConfigureEndpoints(context);
+                    });
+                });
+            }
+            
+        }
+
+        private static void ConfigureSBConsumers(IBusRegistrationConfigurator x)
+        {
+            x.SetKebabCaseEndpointNameFormatter();
+            x.AddConsumer<AddProfileConsumer>(x => { });
+            x.AddConsumer<UpdateProfileConsumer>(x => { });
+            x.AddConsumer<RefreshCacheEventConsumer>(x => { });
         }
     }
 }
